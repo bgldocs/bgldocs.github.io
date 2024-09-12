d5d490dce900657caa329a84bd70240ad72e269a
bgldocs <mhamed.kh@baghdadgamelab.com>
2024-09-12 17:07:51 +0300

    Fix background color

---
+++ assets/nested7-B9_FNmA5.js
@@ -0,0 +1,5 @@
+import"./main-CQbqv8wJ.js";import{m as o}from"./marked.esm-BPQGdaXV.js";o.use({breaks:!0,gfm:!0});fetch("./c3.If.md").then(t=>t.text()).then(t=>{let r=t,n="";for(let e=0;e<r.length;e++)e<r.length-1&&r[e]==`
+`&&r[e+1]==`
+`?n+=` 
+<br>
+`:n+=r[e];document.querySelector(".content").innerHTML=o.parse(n)}).catch(t=>console.error(t));
