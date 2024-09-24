13142e3d99ed8037d9488f905e8c1b90db3b86da
bgldocs <mhamed.kh@baghdadgamelab.com>
2024-09-24 09:43:19 +0300

    Add sound docs

---
+++ assets/nested15-DcKpOJvc.js
@@ -0,0 +1,7 @@
+import"./main-CQbqv8wJ.js";import{m as l}from"./marked.esm-DoEmVC0A.js";l.use({breaks:!0,gfm:!0});fetch("./d8.Sound.md").then(n=>n.text()).then(n=>{let t=n,r="",o=!1;for(let e=0;e<t.length;e++)t[e]=="`"&&t[e+1]=="`"&&t[e+2]=="`"&&t[e+3]!=`
+`?o=!0:t[e]=="`"&&t[e+1]=="`"&&t[e+2]=="`"&&t[e+3]==`
+`&&(o=!1),e<t.length-1&&t[e]==`
+`&&t[e+1]==`
+`&&o==!1?r+=` 
+<br>
+`:r+=t[e];document.querySelector(".content").innerHTML=l.parse(r)}).catch(n=>console.error(n));
